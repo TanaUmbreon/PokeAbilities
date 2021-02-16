ca2959d00dbdbf19e6a416c6d38d7f675ed0e581
Tana <finaltruth.shintai@gmail.com>
2021-02-16 20:54:54 +0900

    ブースターのページを追加。

---
+++ PokeAbilities/Passives/PassiveAbility_2270006.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokeAbilities.Passives
+{
+    /// <summary>
+    /// パッシブ「もらいび」。
+    /// 火傷ダメージを受けず、火傷を受けている間は50%の確率で攻撃ダイスの威力+1。
+    /// </summary>
+    public class PassiveAbility_2270006 : PassiveAbilityBase
+    {
+    }
+}
---
+++ PokeAbilities/Passives/PassiveAbility_2270007.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokeAbilities.Passives
+{
+    /// <summary>
+    /// パッシブ「こんじょう」。
+    /// 状態異常のとき、50%の確率で攻撃ダイスの威力+1。
+    /// </summary>
+    public class PassiveAbility_2270007 : PassiveAbilityBase
+    {
+    }
+}
