9a1cfe8cd0493db6009120c582bea3f51bec20ac
Tana <finaltruth.shintai@gmail.com>
2021-02-18 01:51:52 +0900

    パッシブ「こんじょう」の効果を変更。

--- PokeAbilities/Passives/PassiveAbility_2270007.cs
+++ PokeAbilities/Passives/PassiveAbility_2270007.cs
@@ -6,10 +6,13 @@ namespace PokeAbilities.Passives
 {
     /// <summary>
     /// パッシブ「こんじょう」。
-    /// 状態異常のとき、50%の確率で攻撃ダイスの威力+1。
+    /// 状態異常のとき、虚弱状態の影響を受けず、50%の確率で攻撃ダイスの威力+1。
     /// </summary>
     public class PassiveAbility_2270007 : PassiveAbilityBase
     {
+        public override bool IsImmune(KeywordBuf buf)
+            => buf == KeywordBuf.Weak || base.IsImmune(buf);
+
         public override void BeforeRollDice(BattleDiceBehavior behavior)
         {
             try
