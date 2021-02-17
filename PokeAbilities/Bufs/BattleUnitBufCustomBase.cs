3233ace580c30437307844fa53783ce4d2ed4d05
Tana <finaltruth.shintai@gmail.com>
2021-02-17 23:26:31 +0900

    状態「もらいび」を追加。

---
+++ PokeAbilities/Bufs/BattleUnitBufCustomBase.cs
@@ -0,0 +1,43 @@
+﻿using BaseMod;
+using HarmonyLib;
+using UnityEngine;
+
+namespace PokeAbilities.Bufs
+{
+    /// <summary>
+    /// カスタムしたバフの基底クラスです。
+    /// </summary>
+    public class BattleUnitBufCustomBase : BattleUnitBuf
+    {
+        /// <summary>
+        /// バフの最大スタック数を取得します。
+        /// </summary>
+        protected virtual int MaxStack { get; } = int.MaxValue;
+
+        public override void OnAddBuf()
+            => stack = (stack > MaxStack) ? MaxStack : stack;
+
+        /// <summary>
+        /// <see cref="keywordIconId"/> プロパティの値を使用して、このバフのアイコンを読み込みます。
+        /// </summary>
+        protected void LoadIcon()
+            => LoadIcon(keywordIconId);
+
+        /// <summary>
+        /// 指定したアート ワーク名でこのバフのアイコンを読み込みます。
+        /// </summary>
+        /// <param name="artWorksName">アイコンとして読み込むアート ワーク名。</param>
+        protected void LoadIcon(string artWorksName)
+        {
+            Harmony_Patch.ArtWorks.TryGetValue(artWorksName, out Sprite sprite);
+            if (sprite == null)
+            {
+                Log.Instance.AppendLine("[BattleUnitBufCustomBase.LoadIcon] ArtWorks not found. (artWorksName: " + artWorksName + ")");
+                return;
+            }
+
+            typeof(BattleUnitBuf).GetField("_bufIcon", AccessTools.all).SetValue(this, sprite);
+            typeof(BattleUnitBuf).GetField("_iconInit", AccessTools.all).SetValue(this, true);
+        }
+    }
+}
---
+++ PokeAbilities/Bufs/BattleUnitBuf_FlashFire.cs
@@ -0,0 +1,45 @@
+﻿#pragma warning disable CA1031 // Do not catch general exception types
+
+using System;
+
+namespace PokeAbilities.Bufs
+{
+    /// <summary>
+    /// 状態「もらいび」。
+    /// 火傷ダメージを受けず、50%の確率で攻撃ダイスの威力+1。
+    /// </summary>
+    public class BattleUnitBuf_FlashFire : BattleUnitBufCustomBase
+    {
+        /// <summary>バフの最大スタック数</summary>
+        protected override string keywordId => "FlashFire";
+
+        protected override string keywordIconId => "FlashFireBuf";
+
+        protected override int MaxStack => 1;
+
+        /// <summary>
+        /// <see cref="BattleUnitBuf_FlashFire"/> の新しいインスタンスを生成します。
+        /// </summary>
+        public BattleUnitBuf_FlashFire()
+            => LoadIcon();
+
+        public override bool IsImmune(KeywordBuf buf)
+            => buf == KeywordBuf.Burn || base.IsImmune(buf);
+
+        public override void BeforeRollDice(BattleDiceBehavior behavior)
+        {
+            try
+            {
+                if (!IsAttackDice(behavior.Detail)) { return; }
+                if (RandomUtil.valueForProb >= 0.5f) { return; }
+
+                behavior.ApplyDiceStatBonus(new DiceStatBonus() { power = 1 });
+            }
+            catch (Exception ex)
+            {
+                Log.Instance.AppendLine("[BattleUnitBuf_FlashFire.BeforeRollDice] Exception thrown.");
+                Log.Instance.AppendLine(ex);
+            }
+        }
+    }
+}
