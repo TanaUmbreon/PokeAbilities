ba2f277df4e6bf1c68b9a4248cebda2d193606e9
Tana <finaltruth.shintai@gmail.com>
2021-02-21 15:01:23 +0900

    サンダースのページを追加。

---
+++ PokeAbilities/Passives/PassiveAbility_2270009.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokeAbilities.Passives
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PassiveAbility_2270009 : PassiveAbilityBase
+    {
+    }
+}
