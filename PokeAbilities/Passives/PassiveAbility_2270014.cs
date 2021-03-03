b23462f880319404d166cfaa112580b467e454c2
Tana <finaltruth.shintai@gmail.com>
2021-03-03 23:43:43 +0900

    リーフィアのパッシブを定義。

---
+++ PokeAbilities/Passives/PassiveAbility_2270014.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokeAbilities.Passives
+{
+    /// <summary>
+    /// パッシブ「リーフガード」
+    /// にほんばれ状態のとき、状態異常に対して免疫。
+    /// </summary>
+    public class PassiveAbility_2270014 : PassiveAbilityBase
+    {
+    }
+}
---
+++ PokeAbilities/Passives/PassiveAbility_2270015.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokeAbilities.Passives
+{
+    /// <summary>
+    /// パッシブ「ようりょくそ」
+    /// にほんばれ状態のとき、幕の開始時にクイック2を得る。
+    /// </summary>
+    public class PassiveAbility_2270015 : PassiveAbilityBase
+    {
+    }
+}
