26e49238ee14a90528b3df3d3710b1a7ad6a3da5
Tana <finaltruth.shintai@gmail.com>
2021-03-12 00:30:32 +0900

    コアページ画面でコアページのグループ名を表示するように変更。

---
+++ PokeAbilities/Harmony_Patch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using UI;
+using UnityEngine;
+
+namespace PokeAbilities
+{
+    public class Harmony_Patch
+    {
+        /// <summary>
+        /// <see cref="Harmony_Patch"/> の新しいインスタンスを生成します。
+        /// </summary>
+        public Harmony_Patch()
+        {
+            var harmony = new Harmony("PokeAbilities");
+            MethodInfo method = typeof(Harmony_Patch).GetMethod(nameof(UISpriteDataManager_GetStoryIcon));
+            harmony.Patch(typeof(UISpriteDataManager).GetMethod("GetStoryIcon", AccessTools.all), new HarmonyMethod(method), null, null, null);
+        }
+
+		public static bool UISpriteDataManager_GetStoryIcon(UISpriteDataManager __instance, ref UIIconManager.IconSet __result, string story)
+		{
+            if (story != "PokeAbilities") { return true; }
+
+            BaseMod.Harmony_Patch.ArtWorks.TryGetValue("Pokemon", out var sprite);
+            if (sprite == null) { return true; }
+
+            __result = new UIIconManager.IconSet
+            {
+                icon = sprite,
+                iconGlow = sprite
+            };
+            return false;
+        }
+
+        /// <summary>
+        /// 各 MOD に配置された ArtWorks フォルダー内のスプライトを取得します。
+        /// </summary>
+        public static Dictionary<string, Sprite> ArtWorks
+            => BaseMod.Harmony_Patch.ArtWorks;
+	}
+}
