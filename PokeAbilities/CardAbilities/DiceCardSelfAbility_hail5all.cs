99cd7a0d16e05b56ba54653077c3990b3d93d5b4
Tana <finaltruth.shintai@gmail.com>
2021-03-17 01:12:17 +0900

    バトルページ「あられ」を仮実装。

---
+++ PokeAbilities/CardAbilities/DiceCardSelfAbility_hail5all.cs
@@ -0,0 +1,9 @@
+﻿namespace PokeAbilities.CardAbilities
+{
+    /// <summary>
+    /// [使用時] 全てのキャラクターにあられ5を付与
+    /// </summary>
+    public class DiceCardSelfAbility_hail5all : DiceCardSelfAbilityBase
+    {
+    }
+}
