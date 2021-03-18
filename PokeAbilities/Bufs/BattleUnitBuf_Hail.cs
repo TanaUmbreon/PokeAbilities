fe09e4bd5757c3e0228b0651aa7db8bf22c84c26
Tana <finaltruth.shintai@gmail.com>
2021-03-18 21:53:53 +0900

    バフ「あられ」を定義。

---
+++ PokeAbilities/Bufs/BattleUnitBuf_Hail.cs
@@ -0,0 +1,21 @@
+﻿namespace PokeAbilities.Bufs
+{
+    /// <summary>
+    /// 状態「あられ」。
+    /// 幕の終了時、最大体力の1/16だけダメージを受け(最大5ダメージ)、数値が1減少する(最大5)
+    /// </summary>
+    public class BattleUnitBuf_Hail : BattleUnitBufCustomBase
+    {
+        protected override string keywordId => "Hail";
+
+        protected override string keywordIconId => "HailBuf";
+
+        protected override int MaxStack => 5;
+
+        /// <summary>
+        /// <see cref="BattleUnitBuf_Hail"/> の新しいインスタンスを生成します。
+        /// </summary>
+        public BattleUnitBuf_Hail()
+            => LoadIcon();
+    }
+}
