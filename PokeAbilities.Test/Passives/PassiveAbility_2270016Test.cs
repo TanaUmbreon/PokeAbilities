89f3fbcd2496c288ec72f38ab2ebbaf824490c8a
Tana <finaltruth.shintai@gmail.com>
2021-03-22 01:40:22 +0900

    テストコードを記述。

--- PokeAbilities.Test/Passives/PassiveAbility_2270016Test.cs
+++ PokeAbilities.Test/Passives/PassiveAbility_2270016Test.cs
@@ -9,11 +9,59 @@ namespace PokeAbilities.Test.Passives
     public class PassiveAbility_2270016Test
     {
         private BattleUnitModel owner;
+        private BattleUnitPassiveDetail passiveDetail;
+        private BattleUnitModel enemy;
 
         [SetUp]
         public void SetUp()
         {
             owner = new BattleUnitModelBuilder().ToBattleUnitModel();
+            owner.SetHp(100);
+            passiveDetail = owner.passiveDetail;
+
+            enemy = new BattleUnitModelBuilder() 
+            { 
+                Faction = Faction.Enemy,
+            }.ToBattleUnitModel();
+        }
+
+        [Test]
+        public void TestOnCreated()
+        {
+            var passive = passiveDetail.AddPassive(new PassiveAbility_2270016());
+            passive.OnCreated();
+            Assert.That(passive.GetDamageReductionAll, Is.EqualTo(0));
+            Assert.That(passive.GetBreakDamageReductionAll(0, DamageType.Attack, null), Is.EqualTo(0));
+        }
+
+        [Test(Description = "20%の確率に外れた場合はダメージ軽減数0。")]
+        public void TestBeforeTakeDamage1()
+        {
+            var randomizer = new FixedRandomizer();
+            var passive = passiveDetail.AddPassive(new PassiveAbility_2270016(randomizer));
+            passiveDetail.OnCreated();
+            Assert.That(passive.GetDamageReductionAll, Is.EqualTo(0));
+            Assert.That(passive.GetBreakDamageReductionAll(0, DamageType.Attack, null), Is.EqualTo(0));
+
+            randomizer.ValueForProbReturnValue = 0.2f;
+            passive.BeforeTakeDamage(enemy, 10);
+            Assert.That(passive.GetDamageReductionAll, Is.EqualTo(0));
+            Assert.That(passive.GetBreakDamageReductionAll(0, DamageType.Attack, null), Is.EqualTo(0));
+        }
+
+        [Test(Description = "20%の確率に当たった場合はダメージ軽減数9999。")]
+        public void TestBeforeTakeDamage2()
+        {
+            var randomizer = new FixedRandomizer();
+            var passive = passiveDetail.AddPassive(new PassiveAbility_2270016(randomizer));
+            passiveDetail.OnCreated();
+            Assert.That(passive.GetDamageReductionAll, Is.EqualTo(0));
+            Assert.That(passive.GetBreakDamageReductionAll(0, DamageType.Attack, null), Is.EqualTo(0));
+
+            randomizer.ValueForProbReturnValue = 0.19999f;
+            passive.BeforeTakeDamage(enemy, 10);
+            Assert.That(passive.GetDamageReductionAll, Is.EqualTo(9999));
+            Assert.That(passive.GetBreakDamageReductionAll(0, DamageType.Attack, null), Is.EqualTo(9999));
         }
     }
 }
--- PokeAbilities/Passives/PassiveAbility_2270016.cs
+++ PokeAbilities/Passives/PassiveAbility_2270016.cs
@@ -6,5 +6,39 @@
     /// </summary>
     public class PassiveAbility_2270016 : PassiveAbilityBase
     {
+        /// <summary>被ダメージ軽減量</summary>
+        private int dmgReduction;
+        /// <summary>疑似乱数ジェネレーター</summary>
+        private readonly IRandomizer randomizer;
+
+        /// <summary>
+        /// 既定の疑似乱数ジェネレーターを使用する、
+        /// <see cref="PassiveAbility_2270016"/> の新しいインスタンスを生成します。
+        /// </summary>
+        public PassiveAbility_2270016()
+            : this(DefaultRandomizer.Instance) { }
+
+        /// <summary>
+        /// 指定した疑似乱数ジェネレーターを使用する
+        /// <see cref="PassiveAbility_2270016"/> の新しいインスタンスを生成します。
+        /// </summary>
+        public PassiveAbility_2270016(IRandomizer randomizer) 
+            => this.randomizer = randomizer;
+
+        public override void OnCreated()
+            => dmgReduction = 0;
+
+        public override bool BeforeTakeDamage(BattleUnitModel attacker, int dmg)
+        {
+            dmgReduction = 0;
+
+            return base.BeforeTakeDamage(attacker, dmg);
+        }
+
+        public override int GetDamageReductionAll()
+            => dmgReduction;
+
+        public override int GetBreakDamageReductionAll(int dmg, DamageType dmgType, BattleUnitModel attacker)
+            => dmgReduction;
     }
 }
