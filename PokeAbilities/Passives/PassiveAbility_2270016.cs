1ba4acbadf9eba0e260616317eac2bdb0045dea3
Tana <finaltruth.shintai@gmail.com>
2021-03-21 21:03:38 +0900

    グレイシアのページとパッシブを定義。

---
+++ PokeAbilities/Passives/PassiveAbility_2270016.cs
@@ -0,0 +1,10 @@
+﻿namespace PokeAbilities.Passives
+{
+    /// <summary>
+    /// パッシブ「ゆきがくれ」
+    /// あられ状態のとき、20%の確率でバトルページによるダメージを受けない。あられダメージを受けない。
+    /// </summary>
+    public class PassiveAbility_2270016 : PassiveAbilityBase
+    {
+    }
+}
---
+++ PokeAbilities/Passives/PassiveAbility_2270017.cs
@@ -0,0 +1,10 @@
+﻿namespace PokeAbilities.Passives
+{
+    /// <summary>
+    /// パッシブ「アイスボディ」
+    /// あられ状態のとき、幕の終了時にあられダメージを受けず、最大体力の1/16だけ体力が回復する。(最大5)
+    /// </summary>
+    public class PassiveAbility_2270017 : PassiveAbilityBase
+    {
+    }
+}
