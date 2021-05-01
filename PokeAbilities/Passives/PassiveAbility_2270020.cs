330927f963a736e04f8cbf708414e8a017646790
Tana <finaltruth.shintai@gmail.com>
2021-05-01 19:29:30 +0900

    パッシブ「あめふらし」を実装。

--- PokeAbilities/Passives/PassiveAbility_2270020.cs
+++ PokeAbilities/Passives/PassiveAbility_2270020.cs
@@ -1,4 +1,6 @@
-﻿namespace PokeAbilities.Passives
+﻿using PokeAbilities.Bufs;
+
+namespace PokeAbilities.Passives
 {
     /// <summary>
     /// パッシブ「あめふらし」
@@ -6,5 +8,13 @@
     /// </summary>
     public class PassiveAbility_2270020 : PassiveAbilityBase
     {
+        public override void OnStartBattle()
+        {
+            foreach (BattleUnitModel target in BattleObjectManager.instance.GetAliveList())
+            {
+                target.bufListDetail.RemoveAllWeather();
+                target.bufListDetail.AddBuf<BattleUnitBuf_Rain>(5);
+            }
+        }
     }
 }
