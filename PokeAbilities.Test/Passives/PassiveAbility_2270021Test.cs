1b6ebebad26d0c6052047167107931ab30b38c3e
Tana <finaltruth.shintai@gmail.com>
2021-06-01 23:32:31 +0900

    テストコードを追加。

---
+++ PokeAbilities.Test/Passives/PassiveAbility_2270021Test.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using PokeAbilities.Bufs;
+using PokeAbilities.Passives;
+using PokeAbilities.Test.Helpers;
+
+namespace PokeAbilities.Test.Passives
+{
+    [TestFixture]
+    public class PassiveAbility_2270021Test
+    {
+        private BattleUnitModel owner;
+        private PassiveAbility_2270021 passive;
+
+        [SetUp]
+        public void SetUp()
+        {
+            passive = new PassiveAbility_2270021();
+            owner = new BattleUnitModelBuilder() 
+            {
+                Passives = new[] { passive }
+            }.ToBattleUnitModel();
+        }
+
+        [Test(Description = "あられ状態でない時、何も付与されない。")]
+        public void TestOnRoundStart1()
+        {
+            Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Quickness), Is.Null);
+            Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Quickness), Is.Null);
+
+            passive.OnRoundStart();
+            Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Quickness), Is.Null);
+            Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Quickness), Is.Null);
+        }
+
+        [Test(Description = "あられ状態の時、この幕にクイック2が付与される。")]
+        public void TestOnRoundStart2()
+        {
+            owner.bufListDetail.AddBuf(new BattleUnitBuf_Hail() { stack = 5 });
+            Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Quickness), Is.Null);
+            Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Quickness), Is.Null);
+
+            passive.OnRoundStart();
+            Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Quickness), Is.Not.Null);
+            Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Quickness).stack, Is.EqualTo(2));
+            Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Quickness), Is.Null);
+        }
+    }
+}
---
+++ PokeAbilities.Test/Passives/PassiveAbility_2270022Test.cs
@@ -0,0 +1,105 @@
+﻿using NUnit.Framework;
+using PokeAbilities.Passives;
+using PokeAbilities.Test.Helpers;
+
+namespace PokeAbilities.Test.Passives
+{
+    [TestFixture]
+    public class PassiveAbility_2270022Test
+    {
+        private BattleUnitModel owner;
+        private PassiveAbility_2270022 passive;
+
+        [SetUp]
+        public void SetUp()
+        {
+            passive = new PassiveAbility_2270022();
+            owner = new BattleUnitModelBuilder() 
+            {
+                Passives = new[] { passive }
+            }.ToBattleUnitModel();
+        }
+
+        [Test(Description = "虚弱を受けていない時、何も付与されない。")]
+        public void Test1()
+        {
+            owner.bufListDetail.OnRoundStart();
+            passive.OnRoundStart();
+            Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Quickness), Is.Null);
+            Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Quickness), Is.Null);
+            Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Weak), Is.Null);
+            Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Weak), Is.Null);
+        }
+
+        [Test(Description = "1幕で虚弱を受けるたび、次の幕からクイック1が付与される。")]
+        public void Test2()
+        {
+            for (int round = 1; round <= 7; round++)
+            {
+                owner.bufListDetail.OnRoundStart();
+                passive.OnRoundStart();
+                Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Quickness)?.stack ?? 0, Is.EqualTo(round - 1));
+                Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Quickness), Is.Null);
+                Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Weak), Is.Null);
+                Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Weak), Is.Null);
+
+                owner.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Weak, 1);
+                passive.OnRoundEnd();
+                Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Quickness)?.stack ?? 0, Is.EqualTo(round - 1));
+                Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Quickness), Is.Null);
+                Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Weak).stack, Is.EqualTo(1));
+                Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Weak), Is.Null);
+            }
+        }
+
+        [Test(Description = "1幕で虚弱を複数受けても、次の幕から付与されるクイック数は1。")]
+        public void Test3()
+        {
+            owner.bufListDetail.OnRoundStart();
+            passive.OnRoundStart();
+
+            owner.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Weak, 1);
+            owner.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Weak, 1);
+            passive.OnRoundEnd();
+            Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Quickness), Is.Null);
+            Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Quickness), Is.Null);
+            Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Weak).stack, Is.EqualTo(2));
+            Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Weak), Is.Null);
+
+            owner.bufListDetail.OnRoundStart();
+            passive.OnRoundStart();
+            Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Quickness).stack, Is.EqualTo(1));
+            Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Quickness), Is.Null);
+            Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Weak), Is.Null);
+            Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Weak), Is.Null);
+        }
+
+        [Test(Description = "クイック付与の最大数は6。")]
+        public void Test4()
+        {
+            for (int round = 1; round <= 7; round++)
+            {
+                owner.bufListDetail.OnRoundStart();
+                passive.OnRoundStart();
+                Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Quickness)?.stack ?? 0, Is.EqualTo(round - 1));
+                Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Quickness), Is.Null);
+                Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Weak), Is.Null);
+                Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Weak), Is.Null);
+
+                owner.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Weak, 1);
+                passive.OnRoundEnd();
+                Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Quickness)?.stack ?? 0, Is.EqualTo(round - 1));
+                Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Quickness), Is.Null);
+                Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Weak).stack, Is.EqualTo(1));
+                Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Weak), Is.Null);
+            }
+
+            owner.bufListDetail.OnRoundStart();
+            passive.OnRoundStart();
+            Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Quickness)?.stack ?? 0, Is.EqualTo(6));
+            Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Quickness), Is.Null);
+            Assert.That(owner.bufListDetail.GetActivatedBuf(KeywordBuf.Weak), Is.Null);
+            Assert.That(owner.bufListDetail.GetReadyBuf(KeywordBuf.Weak), Is.Null);
+        }
+    }
+}
