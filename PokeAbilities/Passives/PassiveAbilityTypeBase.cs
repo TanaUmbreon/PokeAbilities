cc10c256bfc1d18ecb7940f9b83708b55b4ac407
Tana <finaltruth.shintai@gmail.com>
2021-06-09 23:54:02 +0900

    タイプ系パッシブを定義。

---
+++ PokeAbilities/Passives/PassiveAbilityTypeBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokeAbilities.Passives
+{
+    /// <summary>
+    /// タイプ系パッシブの基底クラスです。このクラスを継承することでタイプ系パッシブとして扱われます。
+    /// </summary>
+    public abstract class PassiveAbilityTypeBase : PassiveAbilityBase
+    {
+        public abstract IEnumerable<PokeType> Types { get; }
+
+        // ToDo: OnRoundStartAfterメソッドをオーバーライドして、手元のバトルページ2枚にタイプを付与するコードを実装する。
+    }
+}
---
+++ PokeAbilities/Passives/PassiveAbilityTypes.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+
+namespace PokeAbilities.Passives
+{
+    /// <summary>
+    /// パッシブ「ノーマルタイプ」
+    /// このキャラクターはノーマルタイプを持ち、特定のパッシブやバトルページの影響を受ける。
+    /// 幕の開始時、手元のバトルページ2枚に「ノーマルタイプ」をランダムに付与。
+    /// </summary>
+    public class PassiveAbility_22700100 : PassiveAbilityTypeBase
+    {
+        public override IEnumerable<PokeType> Types => new[] { PokeType.Normal, };
+    }
+
+    /// <summary>
+    /// パッシブ「ほのおタイプ」
+    /// このキャラクターはほのおタイプを持ち、特定のパッシブやバトルページの影響を受ける。
+    /// 幕の開始時、手元のバトルページ2枚に「ほのおタイプ」をランダムに付与。
+    /// </summary>
+    public class PassiveAbility_22700200 : PassiveAbilityTypeBase
+    {
+        public override IEnumerable<PokeType> Types => new[] { PokeType.Fire, };
+    }
+
+    /// <summary>
+    /// パッシブ「みずタイプ」
+    /// このキャラクターはみずタイプを持ち、特定のパッシブやバトルページの影響を受ける。
+    /// 幕の開始時、手元のバトルページ2枚に「みずタイプ」をランダムに付与。
+    /// </summary>
+    public class PassiveAbility_22700300 : PassiveAbilityTypeBase
+    {
+        public override IEnumerable<PokeType> Types => new[] { PokeType.Water, };
+    }
+
+    /// <summary>
+    /// パッシブ「みず/ひこうタイプ」
+    /// このキャラクターはみずタイプとひこうタイプを持ち、特定のパッシブやバトルページの影響を受ける。
+    /// 幕の開始時、手元のバトルページ2枚に「みずタイプ」と「ひこうタイプ」をそれぞれランダムに付与。
+    /// </summary>
+    public class PassiveAbility_22700310 : PassiveAbilityTypeBase
+    {
+        public override IEnumerable<PokeType> Types => new[] { PokeType.Water, PokeType.Flying };
+    }
+
+    /// <summary>
+    /// パッシブ「でんきタイプ」
+    /// このキャラクターはでんきタイプを持ち、特定のパッシブやバトルページの影響を受ける。
+    /// 幕の開始時、手元のバトルページ2枚に「でんきタイプ」をランダムに付与。
+    /// </summary>
+    public class PassiveAbility_22700400 : PassiveAbilityTypeBase
+    {
+        public override IEnumerable<PokeType> Types => new[] { PokeType.Electric, };
+    }
+
+    /// <summary>
+    /// パッシブ「くさタイプ」
+    /// このキャラクターはくさタイプを持ち、特定のパッシブやバトルページの影響を受ける。
+    /// 幕の開始時、手元のバトルページ2枚に「くさタイプ」をランダムに付与。
+    /// </summary>
+    public class PassiveAbility_22700500 : PassiveAbilityTypeBase
+    {
+        public override IEnumerable<PokeType> Types => new[] { PokeType.Grass, };
+    }
+
+    /// <summary>
+    /// パッシブ「こおりタイプ」
+    /// このキャラクターはこおりタイプを持ち、特定のパッシブやバトルページの影響を受ける。
+    /// 幕の開始時、手元のバトルページ2枚に「こおりタイプ」をランダムに付与。
+    /// </summary>
+    public class PassiveAbility_22700600 : PassiveAbilityTypeBase
+    {
+        public override IEnumerable<PokeType> Types => new[] { PokeType.Ice, };
+    }
+
+    /// <summary>
+    /// パッシブ「エスパータイプ」
+    /// このキャラクターはエスパータイプを持ち、特定のパッシブやバトルページの影響を受ける。
+    /// 幕の開始時、手元のバトルページ2枚に「エスパータイプ」をランダムに付与。
+    /// </summary>
+    public class PassiveAbility_22701100 : PassiveAbilityTypeBase
+    {
+        public override IEnumerable<PokeType> Types => new[] { PokeType.Psychic, };
+    }
+
+    /// <summary>
+    /// パッシブ「あくタイプ」
+    /// このキャラクターはあくタイプを持ち、特定のパッシブやバトルページの影響を受ける。
+    /// 幕の開始時、手元のバトルページ2枚に「あくタイプ」をランダムに付与。
+    /// </summary>
+    public class PassiveAbility_22701600 : PassiveAbilityTypeBase
+    {
+        public override IEnumerable<PokeType> Types => new[] { PokeType.Dark, };
+    }
+
+    /// <summary>
+    /// パッシブ「フェアリータイプ」
+    /// このキャラクターはフェアリータイプを持ち、特定のパッシブやバトルページの影響を受ける。
+    /// 幕の開始時、手元のバトルページ2枚に「フェアリータイプ」をランダムに付与。
+    /// </summary>
+    public class PassiveAbility_22701800 : PassiveAbilityTypeBase
+    {
+        public override IEnumerable<PokeType> Types => new[] { PokeType.Fairy, };
+    }
+}
