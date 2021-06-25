43b30f60d33e99ff8dcf7ff31332cd6bf486acd9
Tana <finaltruth.shintai@gmail.com>
2021-06-25 19:56:45 +0900

    タイプ系バトルページ状態を実装。

---
+++ PokeAbilities.Test/Bufs/BattleDiceCardBuf_TypeTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PokeAbilities.Bufs;
+
+namespace PokeAbilities.Test.Bufs
+{
+    [TestFixture]
+    public class BattleDiceCardBuf_TypeTest
+    {
+        [Test]
+        public void TestNew()
+        {
+            Assert.That(() => { new BattleDiceCardBuf_Type(PokeType.Normal); }, Throws.Nothing);
+        }
+
+        [Test(Description = "コンストラクタで指定したタイプが Type プロパティの値になる。")]
+        public void TestType()
+        {
+            {
+                var buf = new BattleDiceCardBuf_Type(PokeType.Normal);
+                Assert.That(buf.Type, Is.EqualTo(PokeType.Normal));
+            }
+
+            {
+                var buf = new BattleDiceCardBuf_Type(PokeType.Dark);
+                Assert.That(buf.Type, Is.EqualTo(PokeType.Dark));
+            }
+        }
+
+        [Test(Description = "幕の終了時に破棄される。")]
+        public void TestOnRoundEnd()
+        {
+            var buf = new BattleDiceCardBuf_Type(PokeType.Grass);
+
+            Assert.That(buf.IsDestroyed(), Is.False);
+            buf.OnRoundEnd();
+            Assert.That(buf.IsDestroyed(), Is.True);
+        }
+    }
+}
---
+++ PokeAbilities/Bufs/BattleDiceCardBuf_Type.cs
@@ -0,0 +1,86 @@
+﻿namespace PokeAbilities.Bufs
+{
+    /// <summary>
+    /// バトル ページ状態「○○タイプ」
+    /// ○○タイプを持つキャラクターが使用したとき、このページで与えるダメージ量+1
+    /// </summary>
+    public class BattleDiceCardBuf_Type : BattleDiceCardBufCustomBase
+    {
+        /// <summary>
+        /// タイプを取得します。
+        /// </summary>
+        public PokeType Type { get; private set; }
+
+        protected override string keywordId => _keywordId;
+        private readonly string _keywordId;
+
+        protected override string keywordIconId => _keywordIconId;
+        private readonly string _keywordIconId;
+
+        /// <summary>
+        /// <see cref="BattleDiceCardBuf_Type"/> の新しいインスタンスを生成します。
+        /// </summary>
+        /// <param name="type">付与するタイプ。</param>
+        public BattleDiceCardBuf_Type(PokeType type)
+        {
+            Type = type;
+
+            _keywordId = ToKeywordId(type);
+            _keywordIconId = ToKeywordId(type) + "Buf";
+            LoadIcon();
+        }
+
+        /// <summary>
+        /// 指定されたタイプを <see cref="keywordId"/> 用の文字列に変換します。
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private string ToKeywordId(PokeType value)
+        {
+            switch (value)
+            {
+                case PokeType.Normal:
+                    return "NormalType";
+                case PokeType.Fire:
+                    return "FireType";
+                case PokeType.Water:
+                    return "WaterType";
+                case PokeType.Electric:
+                    return "ElectricType";
+                case PokeType.Grass:
+                    return "GrassType";
+                case PokeType.Ice:
+                    return "IceType";
+                case PokeType.Fighting:
+                    return "FightingType";
+                case PokeType.Poison:
+                    return "PoisonType";
+                case PokeType.Ground:
+                    return "GroundType";
+                case PokeType.Flying:
+                    return "FlyingType";
+                case PokeType.Psychic:
+                    return "PsychicType";
+                case PokeType.Bug:
+                    return "BugType";
+                case PokeType.Rock:
+                    return "RockType";
+                case PokeType.Ghost:
+                    return "GhostType";
+                case PokeType.Dragon:
+                    return "DragonType";
+                case PokeType.Dark:
+                    return "DarkType";
+                case PokeType.Steel:
+                    return "SteelType";
+                case PokeType.Fairy:
+                    return "FairyType";
+                default:
+                    return "Type";
+            }
+        }
+
+        public override void OnRoundEnd()
+            => Destroy();
+    }
+}
