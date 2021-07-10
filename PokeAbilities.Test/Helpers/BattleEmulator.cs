6e454cca05bb33607713f82adb3f5481d8a458a4
Tana <finaltruth.shintai@gmail.com>
2021-07-10 23:08:18 +0900

    戦闘に関わるテストコード実装の一元化と簡略化の為にクラスを追加。

---
+++ PokeAbilities.Test/Helpers/BattleEmulator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using LOR_DiceSystem;
+
+namespace PokeAbilities.Test.Helpers
+{
+    /// <summary>
+    /// 戦闘で行われている処理を疑似的に模倣するユーティリティ クラスです。
+    /// </summary>
+    public static class BattleEmulator
+    {
+        /// <summary>
+        /// 指定したキャラクターに指定した状態をこの幕から付与し、
+        /// <see cref="BattleUnitBuf.OnAddBuf(int)"/> を呼び出します。
+        /// </summary>
+        /// <param name="target">状態を付与する対象のキャラクター。</param>
+        /// <param name="buf">付与する状態。</param>
+        public static void AddBuf(BattleUnitModel target, BattleUnitBuf buf)
+        {
+            target.bufListDetail.AddBuf(buf);
+            buf.OnAddBuf(buf.stack);
+        }
+
+        /// <summary>
+        /// 指定したバトル ダイスの種類で指定したキャラクターに向かって使用する状態のキャラクターを生成します。
+        /// </summary>
+        /// <param name="currentBehaviourDetail">使用するバトル ダイスの種類。</param>
+        /// <param name="target">使用するバトル ダイスの対象となるキャラクター。</param>
+        /// <returns></returns>
+        public static BattleUnitModel CreateAttacker(BehaviourDetail currentBehaviourDetail, BattleUnitModel target)
+        {
+            BattleUnitModel attaker = new BattleUnitModelBuilder()
+            {
+                Faction = Faction.Enemy,
+            }.ToBattleUnitModel();
+
+            attaker.currentDiceAction = new BattlePlayingCardDataInUnitModelBuilder()
+            {
+                Target = target,
+                Card = new BattleDiceCardModelBuilder()
+                {
+                    Owner = attaker,
+                }.ToBattleDiceCardModel(),
+                CurrentBehavior = new BattleDiceBehaviorBuilder()
+                {
+                    Detail = currentBehaviourDetail,
+                }.ToBattleDiceBehavior(),
+            }.ToBattlePlayingCardDataInUnitModel();
+
+            return attaker;
+        }
+    }
+}
---
+++ PokeAbilities.Test/Helpers/BattleInfo.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using PokeAbilities.Bufs;
+
+namespace PokeAbilities.Test.Helpers
+{
+    /// <summary>
+    /// 戦闘で使われるオブジェクトに対する様々な情報を取得するユーティリティ クラスです。
+    /// </summary>
+    public static class BattleInfo
+    {
+        /// <summary>
+        /// 指定したキャラクターの手札の枚数を取得します。
+        /// </summary>
+        /// <param name="target">手札の枚数を取得する対象のキャラクター。</param>
+        /// <returns></returns>
+        public static int GetHandCount(BattleUnitModel target)
+            => target.allyCardDetail.GetHand().Count;
+
+        /// <summary>
+        /// 指定したキャラクターから、何らかのタイプが付与された手札の枚数を取得します。
+        /// </summary>
+        /// <param name="target">手札の枚数を取得する対象のキャラクター。</param>
+        /// <returns></returns>
+        public static int GetTypeHandCount(BattleUnitModel target)
+            => target.allyCardDetail.GetHand().Count(
+                h => h.GetBufList().OfType<BattleDiceCardBuf_Type>().Any());
+
+        /// <summary>
+        /// 指定したキャラクターから、指定したタイプが付与された手札の枚数を取得します。
+        /// </summary>
+        /// <param name="target">手札の枚数を取得する対象のキャラクター。</param>
+        /// <param name="type">手札の枚数を取得する対象のタイプ。</param>
+        /// <returns></returns>
+        public static int GetTypeHandCount(BattleUnitModel target, PokeType type)
+            => target.allyCardDetail.GetHand().Count(
+                h => h.GetBufList().OfType<BattleDiceCardBuf_Type>().Any(b => b.Type == type));
+
+        /// <summary>
+        /// 指定したキャラクターから、何らかのタイプが 2 個以上同時に付与された手札の枚数を取得します。
+        /// </summary>
+        /// <param name="target">手札の枚数を取得する対象のキャラクター。</param>
+        /// <returns></returns>
+        public static int GetMultiTypesHandCount(BattleUnitModel target)
+            => target.allyCardDetail.GetHand().Count(
+                h => h.GetBufList().OfType<BattleDiceCardBuf_Type>().Count() >= 2);
+    }
+}
